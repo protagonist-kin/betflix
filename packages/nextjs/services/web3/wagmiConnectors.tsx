c1b4d9903398506e6a7c2b5ab74851a25942f312
Saurabh Lodha <saurabhlodha221b@gmail.com>
2025-09-28 05:13:34 +0530

    Minor changes in scaffold config

--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -44,7 +44,7 @@ export const wagmiConnectors = () => {
     ],
 
     {
-      appName: "scaffold-eth-2",
+      appName: "Betflix",
       projectId: scaffoldConfig.walletConnectProjectId,
     },
   );
